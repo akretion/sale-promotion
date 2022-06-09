dd2afaefdfee7af1e2c883343697ef8d913a91d5
Florian Mounier <paradoxxx.zero@gmail.com>
2022-06-09 16:59:09 +0200

    [ADD] sale_stock_coupon_apply_on_domain

---
+++ sale_stock_coupon_apply_on_domain/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 Akretion - Florian Mounier
+{
+    "name": "Sale Stock Coupon Apply On Domain",
+    "summary": "This module makes sale_stock_coupon work with sale_coupon_apply_on_domain.",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "website": "https://github.com/OCA/sale-promotion",
+    "depends": ["sale_stock_coupon", "sale_coupon_apply_on_domain"],
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "data": [],
+    "auto_install": True,
+}
