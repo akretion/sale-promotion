01a21f3375dd0d142e31b61890846cfe77e45d85
Florian Mounier <paradoxxx.zero@gmail.com>
2022-09-07 10:37:15 +0200

    [ADD] sale_coupon_invoice_delivered_apply_on_domain

---
+++ sale_coupon_invoice_delivered_apply_on_domain/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 Akretion - Florian Mounier
+{
+    "name": "Sale Coupon Invoice Delivered Apply On Domain",
+    "summary": "This module makes sale_coupon_invoice_delivered work with sale_coupon_apply_on_domain.",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "website": "https://github.com/OCA/sale-promotion",
+    "depends": ["sale_coupon_invoice_delivered", "sale_coupon_apply_on_domain"],
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "data": [],
+    "auto_install": True,
+}
